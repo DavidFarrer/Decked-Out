5ee1ea10c320fa967b2b8e8b6363aa0dcd5d72d4
Dave Farrer <dave.farrer89@gmail.com>
2018-04-30 15:55:08 -0400

    add basename for react router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(rootReducer);
 
 ReactDOM.render((
 	<Provider store={store}>
-		<BrowserRouter>
+		<BrowserRouter basename="/Online-Outfitters">
 			<App />
 		</BrowserRouter>
 	</Provider>
